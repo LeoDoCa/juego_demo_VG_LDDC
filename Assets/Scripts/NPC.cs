2b618bbf24d70d3476d34d0c57f2eaad5a94775c
LeoDoCa <20223tn049@utez.edu.mx>
2025-10-15 18:55:08 -0600

    [ADD] Agregué lo de la practica del 15 de octubre

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -5,11 +6,40 @@ public class NPC : MonoBehaviour
 {
     private NavMeshAgent  agent;
     public Transform destination;
+    private Collider[] detectedObjects =  new Collider[10];
+    public float detectionRadius = 10f;
+    private Transform player;
     
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         agent.destination = destination.position;
+    }
+
+    private void FixedUpdate()
+    {
+        var numberOfColliders = Physics.OverlapSphereNonAlloc(transform.position, detectionRadius, detectedObjects);
+        player = null;
+        for (int i = 0; i < numberOfColliders; i++)
+        {
+            var col = detectedObjects[i];
+            if (!col.CompareTag("Player")) continue;
+            player = col.transform;
+            var vectorToPlayer = player.position - transform.position;
+            var dot = Vector3.Dot(vectorToPlayer, transform.forward);
+            if (dot < 0) continue;
+            
+            Physics.Raycast(transform.position, vectorToPlayer, out var hit);
+            if (hit.collider.transform != player) continue;
+            
+            agent.destination = player.position;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(0.04f, 0.92f, 1f, 0.4f);
+        Gizmos.DrawSphere(transform.position, detectionRadius);
     }
 
     void Update()
